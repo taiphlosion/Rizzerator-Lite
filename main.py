f5cd9bb9ed34cb80301239106f5d91fe6bea725f
Tai Tran <tai.tran@ufl.edu>
2023-04-19 16:21:15 -0400

    Focus on preprocessing file

--- main.py
+++
@@ -1,9 +0,0 @@
-#poop butt fart
-import pandas as pd
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
-
-words = pd.read('unigram_freq.csv')
-words['']
