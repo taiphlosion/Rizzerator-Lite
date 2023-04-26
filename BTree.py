9702abca5ca99ab1eca5ed9345fb553296c2b9cf
Tai Tran <tai.tran@ufl.edu>
2023-04-25 22:17:24 -0400

    Got the B tree working, testing it out

--- BTree.py
+++ BTree.py
@@ -1,70 +1,89 @@
-class BTreeNode:
-    #Constructor for the object (tree node)
-    def __init__(self, degree, leaf=True):
-        # Set at 3 for the tree
-        self.degree = degree
-        # Dictionary to hold words and their values
-        # Syntax: "self.key[word] = confidence_score"
-        self.rizzionary = {}
-        # List to hold the children
-        self.children = []
-        #bool to check if leaf, leaf have children, if not, no children
-        self.leaf = leaf
+class Node:
+    def __init__(self, t, is_leaf=True):
+        self.t = t  # the minimum degree of the node
+        self.keys = []  # a list of keys (confidence score values)
+        self.words = []  # a list of associated words for each key
+        self.children = []  # a list of child nodes
+        self.is_leaf = is_leaf
+
+    def search(self, key):
+        i = 0
+        while i < len(self.keys) and key > self.keys[i]:
+            i += 1
+        if i < len(self.keys) and key == self.keys[i]:
+            return self.words[i]
+        elif self.is_leaf:
+            return None
+        else:
+            return self.children[i].search(key)
 
     def split_child(self, i, child):
-
-        new_child = BTreeNode(leaf=child.leaf)
-        mid = (len(child.keys) - 1) // 2
+        new_child = Node(self.t, is_leaf=child.is_leaf)
+        mid = self.t - 1
         new_key = child.keys[mid]
-        new_value = child.keys[mid]
-        self.keys[new_key] = new_value
+        new_words = child.words[mid:]
+
+        self.keys.insert(i, new_key)
+        self.words.insert(i, new_words)
         self.children.insert(i + 1, new_child)
-        new_child.keys = {k: child.keys[k] for k in list(child.keys)[mid+1:]}
-        child.keys = {k: child.keys[k] for k in list(child.keys)[:mid]}
-        if not child.leaf:
+
+        new_child.keys = child.keys[mid+1:]
+        new_child.words = child.words[mid+1:]
+
+        child.keys = child.keys[:mid]
+        child.words = child.words[:mid]
+
+        if not child.is_leaf:
             new_child.children = child.children[mid+1:]
             child.children = child.children[:mid+1]
 
-    def insert_non_full(self, key, value):
-        i = len(self.keys) - 1
-        if self.leaf:
-            self.keys[key] = value
-            while i >= 0 and key < list(self.keys.keys())[i]:
-                self.keys[list(self.keys.keys())[i+1]] = self.keys[list(self.keys.keys())[i]]
-                i -= 1
-            self.keys[list(self.keys.keys())[i+1]] = key
-        else:
-            while i >= 0 and key < list(self.keys.keys())[i]:
-                i -= 1
-            i += 1
-            if len(self.children[i].keys) == self.degree:
-                self.split_child(i, self.children[i])
-                if key > list(self.keys.keys())[i]:
-                    i += 1
-            self.children[i].insert_non_full(key, value)
-
 class BTree:
-    def __init__(self, degree=3):
-        self.root = BTreeNode(degree, leaf=True)
+    def __init__(self, t):
+        self.root = Node(t, is_leaf=True)
+        self.t = t
 
     def search(self, key):
-        return self._search(key, self.root)
+        return self.root.search(key)
 
-    def _search(self, key, node):
-        if key in node.keys:
-            return node.keys[key]
-        elif node.leaf:
-            return None
-        else:
-            i = 0
-            while i < len(list(node.keys.keys())) and key > list(node.keys.keys())[i]:
-                i += 1
-            return self._search(key, node.children[i])
-
-    def insert(self, key, value):
-        if len(self.root.keys) == self.root.degree:
-            new_root = BTreeNode(self.root.degree, leaf=False)
+    def insert(self, key, words):
+        if len(self.root.keys) == 2*self.t - 1:
+            new_root = Node(self.t, is_leaf=False)
             new_root.children.append(self.root)
             new_root.split_child(0, self.root)
             self.root = new_root
-        self.root.insert_non_full(key, value)
+        self._insert_non_full(self.root, key, words)
+
+    def _insert_non_full(self, node, key, words):
+        i = len(node.keys) - 1
+        if node.is_leaf:
+            while i >= 0 and key < node.keys[i]:
+                i -= 1
+            node.keys.insert(i+1, key)
+            node.words.insert(i+1, words)
+        else:
+            while i >= 0 and key < node.keys[i]:
+                i -= 1
+            i += 1
+            if len(node.children[i].keys) == 2*self.t - 1:
+                node.split_child(i, node.children[i])
+                if key > node.keys[i]:
+                    i += 1
+            self._insert_non_full(node.children[i], key, words)
+
+
+# Create a new tree
+tree = BTree(3)
+
+# Insert some key-value pairs
+tree.insert(1.5, ["apple", "ass", "bob", "suckas"])
+tree.insert(1.7, "banana")
+tree.insert(1.8, ["grape", "trees", "glass", "dirt"])
+tree.insert(1.9, "kiwi")
+
+# Search for a key
+result = tree.search(0.8)
+print(result)  # Output: ['pear', 'grape']
+
+# Search for a deleted key
+result = tree.search(0.5)
+print(result)  # Output: None
